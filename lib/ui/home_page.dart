69a688acd8a1b871eea7ee9a76ca16b95fbfa15a
Marcos Merlini <merlinimarcos@gmail.com>
2019-09-26 13:06:12 -0300

    Adicionado campo de pesquisa tema n funciona
    Adicionado campo de pesquisa e tema(n esta funcionando)

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -8,17 +8,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
   int _offset = 0;
 
   Future<Map> _getGifs() async {
     http.Response response;
 
-    if(_search == null) {
-      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=P4fqOb8pLMyXD7zKjjQNK0dNaXlvKlWc&limit=20&rating=G");
+    if (_search == null) {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=P4fqOb8pLMyXD7zKjjQNK0dNaXlvKlWc&limit=20&rating=G");
     } else {
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=P4fqOb8pLMyXD7zKjjQNK0dNaXlvKlWc&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=P4fqOb8pLMyXD7zKjjQNK0dNaXlvKlWc&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
     }
 
     return json.decode(response.body);
@@ -36,8 +37,28 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Image.network(
+              "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.black,
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Pesquise Aqui!",
+                    labelStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder()),
+                style: TextStyle(color: Colors.white, fontSize: 18.0),
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ));
   }
 }
